6e74d634138b21057ff763ec64d1141e1a9d12c5
Nazar <nazar9505@gmail.com>
2015-02-21 13:46:59 +0500

    error

--- Api.php
+++ Api.php
@@ -1,111 +1,76 @@
 <?php
 
-function mpr($data)
-{
-	echo "<pre>";
-	print_r($data);
-	echo "</pre>";
-}
+class Api{
 
-class App
-{
-	const API_VERSION = '5.24';
-	const CALLBACK_BLANK = 'https://oauth.vk.com/blank.html';
-	const AUTHORIZE_URL = 'https://oauth.vk.com/authorize?client_id={client_id}&scope={scope}&redirect_uri={redirect_uri}&display={display}&v=5.24&response_type={response_type}';
-	const GET_TOKEN_URL = 'https://oauth.vk.com/access_token?client_id={client_id}&client_secret={client_secret}&code={code}&redirect_uri={redirect_uri}';
-	const METHOD_URL = 'https://api.vk.com/method/';
-
-	public $secret_key = null;
-	public $scope = array();
-	public $client_id = null;
-	public $access_token = null;
-	public $token = null;
-
-	public function __construct($options = array())
+	public static function getUserInfo($token, $id_user)
 	{
-		$this->scope[]='offline';
-		$this->scope[]='groups';
-		if(count($options) > 0)
+		$url = App::METHOD_URL.'users.get?user_id='.$id_user.
+				'&fields=sex, bdate, city, country, photo_50, photo_100, photo_200_orig, photo_200, photo_400_orig, photo_max, photo_max_orig, photo_id, online, online_mobile, domain, has_mobile, contacts, connections, site, education, universities, schools, can_post, can_see_all_posts, can_see_audio, can_write_private_message, status, last_seen, common_count, relation, relatives, counters, screen_name, maiden_name, timezone, occupation,activities, interests, music, movies, tv, books, games, about, quotes, personal, friends_status&v='.App::API_VERSION.'&access_token='.$token;
+		$arUser = App::call($url)[0];
+		return new User($arUser);
+	}
+
+	public static function getUserAdminGroupsName($token, $id_user, $offset = 0)
+	{
+		if ($offset <= 1000)
 		{
-			foreach($options as $key => $value)
+			$url = App::METHOD_URL.'groups.get?user_id='.$id_user.
+					'&offset='.$offset.'&extended=1&fields=city, country, place, description, wiki_page, members_count, counters, start_date, finish_date, can_post, can_see_all_posts, activity, status, contacts, links, fixed_post, verified, site, can_create_topic&offset=0&count=1000&v='.App::API_VERSION.'&access_token='.$token;
+			$arGroups = App::call($url);
+			$adminGroups = array();
+			foreach ($arGroups['items'] as $key => $group)
 			{
-				if($key == 'scope' && is_string($value))
+				if ($group['is_admin'] == 1 && $group['admin_level'] == 3)
 				{
-					$_scope = explode(',', $value);
-					$this->scope = array_merge($this->scope, $_scope);
-				}
-				else
-				{
-					$this->$key = $value;
+					$adminGroups[] = $group['name'];
 				}
 			}
+			return $adminGroups;
 		}
-	}
-
-	public function get_code_token($type="code")
-	{
-		$url = self::AUTHORIZE_URL;
-		$scope = implode(',', $this->scope);
-		$url = str_replace('{client_id}', $this->client_id, $url);
-		$url = str_replace('{scope}', $scope, $url);
-		$url = str_replace('{redirect_uri}', self::CALLBACK_BLANK, $url);
-		$url = str_replace('{display}', 'page', $url);
-		$url = str_replace('{response_type}', $type, $url);
-		return $url;
-   }
-
-   public function get_token($code)
-   {
-   	$url = self::GET_TOKEN_URL;
-   	$url = str_replace('{code}', $code, $url);
-   	$url = str_replace('{client_id}', $this->client_id, $url);
-   	$url = str_replace('{client_secret}', $this->secret_key, $url);
-   	$url = str_replace('{redirect_uri}', self::CALLBACK_BLANK, $url);
-   	return $this->call($url);
-   }
-
-   function call($url = '')
-   {
-		if(function_exists('curl_init'))
-			$json = $this->curl_post($url);
 		else
-			$json = file_get_contents($url);
-		$json = json_decode($json, true);
-		if(isset($json['response']))
-			return $json['response'];
-		return $json;
-	}
-
-	private function curl_get($url)
-	{
-		if(!function_exists('curl_init'))
-			return false;
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$tmp = curl_exec ($ch);
-		curl_close ($ch);
-		$tmp = preg_replace('/(?s)<meta http-equiv="Expires"[^>]*>/i', '', $tmp);
-		return $tmp;
-	}
-
-	private function curl_post($url)
-	{
-		if(!function_exists('curl_init'))
-			return false;
-		$param = parse_url($url);
-		if( $curl = curl_init() )
 		{
-			curl_setopt($curl, CURLOPT_URL, $param['scheme'].'://'.$param['host'].$param['path']);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl, CURLOPT_HEADER, 0);
-			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $param['query']);
-			$out = curl_exec($curl);
-			curl_close($curl);
-			return $out;
+			throw new Exception('Смещение должно быть меньше или равно 1000!');
 		}
-		return false;
 	}
+
+	public static function getGroupTopics($token, $domain, $offset = 0)
+	{
+		if ($offset <= 100)
+		{
+			$url = App::METHOD_URL.'wall.get?domain='.$domain.
+				'&offset='.$offset.'&count=100&filter=all&v='.App::API_VERSION.'&access_token='.$token;
+			$arPosts = App::call($url);
+			$posts = array();
+			foreach ($arPosts['items'] as $key => $post)
+			{
+				$posts[] = new Post($post);
+			}
+			return $posts;
+		}
+		else
+		{
+			throw new Exception('Смещение должно быть меньше или равно 100!');
+		}
+	}
+
+	public static function getGroupTopicComments($token, $domain, $post_id, $offset = 0)
+	{
+		if ($offset <= 100)
+		{
+			$url = App::METHOD_URL.'wall.getComments?owner_id=-'.$domain.
+				'&post_id='.$post_id.'&offset='.$offset.'&count=100&filter=all&v='.App::API_VERSION.'&access_token='.$token;
+			$arComments = App::call($url);
+			$comments = array();
+			foreach ($arComments['items'] as $key => $comment)
+			{
+				$comments[] = new Comment($comment);
+			}
+			return $comments;
+		}
+		else
+		{
+			throw new Exception('Смещение должно быть меньше или равно 100!');
+		}
+	}
+
 }
