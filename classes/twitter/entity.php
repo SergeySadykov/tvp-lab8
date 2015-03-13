92e44a2c8cea26bf3183cd0294cbc551a5adc708
toktarevvasily <vylondeor@yandex.ru>
2015-03-14 01:31:34 +0500

    Добавил классы для сущностей

---
+++ classes/twitter/entity.php
@@ -0,0 +1,68 @@
+<?
+	/**
+	* Entities provide metadata and additional contextual
+	* information about content posted on Twitter.
+	* Entities are never divorced from the content they describe.
+	*/
+	class Entity
+	{
+		public $hashtags;
+		public $media;
+		public $urls;
+		public $userMentions;
+	}
+
+	class Hashtags
+	{
+		public $indices;
+		public $text;
+	}
+
+	class Media
+	{
+		public $displayUrl;
+		public $expandedUrl;
+		public $id;
+		public $idStr;
+		public $indices;
+		public $mediaUrl;
+		public $mediaUrlHttps;
+		public $sizes;
+		public $sourceStatusId;
+		public $sourceStatusIdStr;
+		public $type;
+		public $url;
+	}
+
+	class Size
+	{
+		public $h;
+		public $resize;
+		public $w;
+	}
+
+	class Sizes
+	{
+		public $thumb;
+		public $large;
+		public $medium;
+		public $small;
+	}
+
+	class Url
+	{
+		public $displayUrl;
+		public $expandedUrl;
+		public $indices;
+		public $url;
+	}
+
+	class UserMention
+	{
+		public $id;
+		public $idStr;
+		public $indices;
+		public $name;
+		public $screenName;
+	}
+?>
