4b66c48f10c37af056fe8350b662d1f641514fb2
toktarevvasily <vylondeor@yandex.ru>
2015-03-14 01:11:13 +0500

    Добавил класс твита

---
+++ classes/twitter/tweet.php
@@ -0,0 +1,36 @@
+<?
+	class Tweet
+	{
+		public $annotations;
+		public $contributors;
+		public $coordinates;
+		public $createdAt;
+		public $currentUserRetweet;
+		public $entities;
+		public $favoriteCount;
+		public $favorited;
+		public $filterLevel;
+		public $geo;
+		public $id;
+		public $idStr;
+		public $inReplyToScreenName;
+		public $inReplyToStatusId;
+		public $inReplyToStatusIdStr;
+		public $inReplyToUserId;
+		public $inReplyToUserIdStr;
+		public $lang;
+		public $places;
+		public $posiblySensitive;
+		public $scopes;
+		public $retweetCount;
+		public $retweeted;
+		public $retweetedStatus;
+		public $source;
+		public $text;
+		public $truncated;
+		public $user;
+		public $withheldCopyright;
+		public $withheldInCountries;
+		public $withheldScope;
+	}
+?>
