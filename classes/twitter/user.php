daec99824006967521af92d45f604c1d09bb9118
toktarevvasily <vylondeor@yandex.ru>
2015-03-14 01:02:11 +0500

    Добавил класс пользователя

---
+++ classes/twitter/user.php
@@ -0,0 +1,47 @@
+<?
+	class User{
+		public $contributorsEnabled;
+		public $createdAt;
+		public $defaultProfile;
+		public $defaultProfileImage;
+		public $description;
+		public $entities;
+		public $favouritesCount;
+		public $followRequestSent;
+		public $following;
+		public $followersCount;
+		public $friendsCount;
+		public $geoEnabled;
+		public $id;
+		public $idStr;
+		public $isTranslator;
+		public $lang;
+		public $listedCount;
+		public $location;
+		public $name;
+		public $notifications;
+		public $profileBackgroundColor;
+		public $profileBackgroundImageUrl;
+		public $profileBackgroundImageUrlHttps;
+		public $profileBackgroundTitle;
+		public $profileBannerUrl;
+		public $profileImageUrl;
+		public $profileImageUrlHttps;
+		public $profileLinkColor;
+		public $profileSidebarBorderColor;
+		public $profileSidebarFillColor;
+		public $profileTextColor;
+		public $profileUseBackgroundImage;
+		public $protected;
+		public $screenName;
+		public $showAllInlineMedia;
+		public $status;
+		public $statusesCount;
+		public $timeZone;
+		public $url;
+		public $utcOffset;
+		public $verified;
+		public $withheldInCountries;
+		public $withheldScope;
+	}
+?>
