11120e5388f2badd3d8d55b6add630d6a7e0aebe
NikonorovNikita <nikonorov.nikita@gmail.com>
2015-03-20 21:56:56 +0500

    Посты на стену и новый дизайн

---
+++ Nikonorov/vk-v0.2/custom.js
@@ -0,0 +1,27 @@
+$(function()
+{
+	$('#post_btn').click(function(){
+		var
+			uid = $('#post_user_id').val(),
+			gid = $('#post_group_id').val(),
+			message = $('#post_text').val(),
+			attachments = $('#post_image').val();
+
+		$.ajax(
+		{
+			async: false,
+		  	type: 'POST',
+		  	url: 'post.php',
+		  	data: 'uid='+uid+'&gid='+uid+'&message='+message+'&attachments='+attachments+'',
+		  	success: function(msg)
+		  	{
+			//console.log(msg);
+			
+		   	alert('Запись добавлена!')
+		   	window.location = '/'
+		  	}
+		})
+
+		return false
+	})
+})
